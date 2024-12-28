036a876ccc6e8a1db9c8d1ed4b4a8584a84c04f1
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-28 21:53:09 +0200

    Add: User Model

---
+++ src/models/user.model.js
@@ -0,0 +1,50 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["public_user", "investigator", "admin"],
+    default: "public_user",
+  },
+  profilePicture: {
+    type: String,
+    default: "",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+userSchema.pre("save", function (next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+export const User = model("User", userSchema);
