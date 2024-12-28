efd09fe4232fb54b91d9c60864ca8e84ab6eb2fa
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-29 01:38:28 +0200

    Edit: Auth Middleware To Have JWT

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,7 @@
-export const authMiddleware = (req, res, next) => {
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+export const authMiddleware = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
@@ -18,6 +21,14 @@ export const authMiddleware = (req, res, next) => {
   const token = spiltted[1];
 
   try {
+    const payload = await jwt.verify(token, process.env.JWT_SECRET);
+
+    const id = payload.id;
+
+    const user = await User.findById(id);
+
+    req.user = user;
+
     next();
   } catch (error) {
     return res.status(500).send({
