63a3498fbbcc166ce5b7e8b28e7f07d75735e61b
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-29 23:08:10 +0200

    Add: Get Case Route

--- src/routes/case.routes.js
+++ src/routes/case.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { createCase, getCases } from "../controllers/case.controller.js";
+import {
+  createCase,
+  getCases,
+  getCase,
+} from "../controllers/case.controller.js";
 
 const caseRouter = new Router();
 
 caseRouter.post("/", createCase);
 caseRouter.get("/", getCases);
+caseRouter.get("/:id?", getCase);
 
 export default caseRouter;
