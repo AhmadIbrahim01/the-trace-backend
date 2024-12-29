b4131c606e64926d58be914cec190180c25f3448
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-30 00:38:16 +0200

    Add: Path To Get Witnesses API

--- src/routes/witness.routes.js
+++ src/routes/witness.routes.js
@@ -3,11 +3,13 @@ import { Router } from "express";
 import {
   addWitness,
   updateWitness,
+  getWitnesses,
 } from "../controllers/witness.controller.js";
 
 const witnessRouter = new Router();
 
 witnessRouter.post("/:id?", addWitness);
 witnessRouter.put("/case/:caseId/witnesses/:witnessId?", updateWitness);
+witnessRouter.get("/:id?", getWitnesses);
 
 export default witnessRouter;
