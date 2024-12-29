4851eafcd8a2291f32a74feb68218fbfe24f97ff
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-29 22:57:37 +0200

    Add: Path To Witness Router

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import connectToDatabase from "./db/connection.js";
 import authRouter from "./routes/auth.routes.js";
 import testimonialRouter from "./routes/testimonial.routes.js";
 import caseRouter from "./routes/case.routes.js";
+import witnessRouter from "./routes/witness.routes.js";
 
 const app = express();
 
@@ -20,6 +21,7 @@ app.use(
 app.use("/auth", authRouter);
 app.use("/testimonial", testimonialRouter);
 app.use("/case", caseRouter);
+app.use("/witness", witnessRouter);
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Server running on port ${process.env.SERVER_PORT}`);
