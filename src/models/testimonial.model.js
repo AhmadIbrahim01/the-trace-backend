2205ff89f6a7216e98a72b414bfcb9bf6df887b5
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-29 22:33:04 +0200

    Edit: Timestamp In Testimonial Model

--- src/models/testimonial.model.js
+++ src/models/testimonial.model.js
@@ -1,18 +1,20 @@
 import { Schema, model } from "mongoose";
 
-const testimonialSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
+const testimonialSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    testimonial: {
+      type: String,
+      required: true,
+    },
+    profilePhoto: {
+      type: String,
+    },
   },
-  testimonial: {
-    type: String,
-    required: true,
-  },
-  profilePhoto: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 export const Testimonial = model("Testimonial", testimonialSchema);
