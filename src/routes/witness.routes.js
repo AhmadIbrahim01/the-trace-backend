8345ee7968172e2b6feffd602306059c47fe8253
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-30 00:21:26 +0200

    Add: Update Witness API Route

--- src/routes/witness.routes.js
+++ src/routes/witness.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 
-import { addWitness } from "../controllers/witness.controller.js";
+import {
+  addWitness,
+  updateWitness,
+} from "../controllers/witness.controller.js";
 
 const witnessRouter = new Router();
 
 witnessRouter.post("/:id?", addWitness);
+witnessRouter.put("/case/:caseId/witnesses/:witnessId?", updateWitness);
 
 export default witnessRouter;
