27df4be4066bd99ced41455a67ef3cf897c031ed
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-30 23:36:00 +0200

    Add: Get Tip API Route

--- src/routes/tip.routes.js
+++ src/routes/tip.routes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
 
-import { addTip, updateTip, getTips } from "../controllers/tip.controller.js";
+import {
+  addTip,
+  updateTip,
+  getTips,
+  getTip,
+} from "../controllers/tip.controller.js";
 
 const tipRouter = new Router();
 
 tipRouter.post("/:caseId?", addTip);
 tipRouter.put("/:caseId/:tipId?", updateTip);
 tipRouter.get("/:caseId?", getTips);
+tipRouter.get("/:caseId/:tipId?", getTip);
 
 export default tipRouter;
