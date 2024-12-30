12e43228f0ec45879fce110451689f947f182612
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-30 18:22:34 +0200

    Add: Get Evidence API Route

--- src/routes/evidence.routes.js
+++ src/routes/evidence.routes.js
@@ -3,12 +3,14 @@ import {
   addEvidence,
   updateEvidence,
   getEvidences,
+  getEvidence,
 } from "../controllers/evidence.controller.js";
 
 const evidenceRouter = new Router();
 
 evidenceRouter.post("/:caseId?", addEvidence);
 evidenceRouter.put("/:caseId/:evidenceId?", updateEvidence);
-evidenceRouter.get("/:caseId", getEvidences);
+evidenceRouter.get("/:caseId?", getEvidences);
+evidenceRouter.get("/:caseId/:evidenceId?", getEvidence);
 
 export default evidenceRouter;
