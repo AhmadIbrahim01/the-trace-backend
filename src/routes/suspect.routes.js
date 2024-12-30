d31dc18e2124433885237f7f131f46fadde1ae1a
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-30 03:53:27 +0200

    Add: Delete Suspect API Path

--- src/routes/suspect.routes.js
+++ src/routes/suspect.routes.js
@@ -4,6 +4,7 @@ import {
   updateSuspect,
   getSuspects,
   getSuspect,
+  deleteSuspect,
 } from "../controllers/suspect.controller.js";
 const suspectRouter = new Router();
 
@@ -11,5 +12,6 @@ suspectRouter.post("/:caseId?", addSuspect);
 suspectRouter.put("/:caseId/:suspectId?", updateSuspect);
 suspectRouter.get("/:caseId?", getSuspects);
 suspectRouter.get("/:caseId/:suspectId?", getSuspect);
+suspectRouter.delete("/:caseId/:suspectId?", deleteSuspect);
 
 export default suspectRouter;
