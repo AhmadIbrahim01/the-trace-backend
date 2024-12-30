ccdffdbc3f006fcadb45dc4144e1eb1d9ffd6cd4
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-30 18:31:18 +0200

    Add: Delete Evidence API Route

--- src/routes/evidence.routes.js
+++ src/routes/evidence.routes.js
@@ -4,6 +4,7 @@ import {
   updateEvidence,
   getEvidences,
   getEvidence,
+  deleteEvidence,
 } from "../controllers/evidence.controller.js";
 
 const evidenceRouter = new Router();
@@ -12,5 +13,6 @@ evidenceRouter.post("/:caseId?", addEvidence);
 evidenceRouter.put("/:caseId/:evidenceId?", updateEvidence);
 evidenceRouter.get("/:caseId?", getEvidences);
 evidenceRouter.get("/:caseId/:evidenceId?", getEvidence);
+evidenceRouter.delete("/:caseId/:evidenceId?", deleteEvidence);
 
 export default evidenceRouter;
