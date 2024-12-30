6c5ed82339fd5bb36823bbe9c1062363813602d6
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-30 19:37:35 +0200

    Add: Get Comments API Route

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   createComment,
   deleteComment,
+  getComments,
 } from "../controllers/comment.controller.js";
 
 const commentRouter = new Router();
 
 commentRouter.post("/:caseId?", createComment);
 commentRouter.delete("/:caseId/:commentId?", deleteComment);
+commentRouter.get("/:caseId?", getComments);
 
 export default commentRouter;
