f74532c5158bc6ea4cf44e266ae0c53c20515ec1
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-31 00:17:03 +0200

    Add: Add Anonymous Tip Route

--- src/routes/tip.routes.js
+++ src/routes/tip.routes.js
@@ -8,11 +8,13 @@ import {
   deleteTip,
   deleteTips,
   toggleTip,
+  addAnonymousTip,
 } from "../controllers/tip.controller.js";
 
 const tipRouter = new Router();
 
 tipRouter.post("/:caseId?", addTip);
+tipRouter.post("/anony/:caseId?", addAnonymousTip);
 tipRouter.put("/:caseId/:tipId?", updateTip);
 tipRouter.get("/:caseId?", getTips);
 tipRouter.get("/:caseId/:tipId?", getTip);
