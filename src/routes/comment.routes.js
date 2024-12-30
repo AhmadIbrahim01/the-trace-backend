8cf5c59431b3f471bbabba1946f4b07b991d596b
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-30 20:54:42 +0200

    Add: Like Comment API Route

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -4,6 +4,7 @@ import {
   deleteComment,
   getComments,
   getComment,
+  likeComment,
 } from "../controllers/comment.controller.js";
 
 const commentRouter = new Router();
@@ -12,5 +13,6 @@ commentRouter.post("/:caseId?", createComment);
 commentRouter.delete("/:caseId/:commentId?", deleteComment);
 commentRouter.get("/:caseId?", getComments);
 commentRouter.get("/:caseId/:commentId?", getComment);
+commentRouter.post("/:caseId/:commentId?", likeComment);
 
 export default commentRouter;
