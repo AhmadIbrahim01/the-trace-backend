bc4387436dd0469bbf6d495a9957f2c46fc91532
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-30 19:28:45 +0200

    Add: Delete Comment API Route

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { createComment } from "../controllers/comment.controller.js";
+import {
+  createComment,
+  deleteComment,
+} from "../controllers/comment.controller.js";
 
 const commentRouter = new Router();
 
 commentRouter.post("/:caseId?", createComment);
+commentRouter.delete("/:caseId/:commentId?", deleteComment);
 
 export default commentRouter;
