53f8f00ee12a2b36b13f7aacf6df314913d4c59e
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-30 02:03:47 +0200

    Edit: Witness Routes

--- src/routes/witness.routes.js
+++ src/routes/witness.routes.js
@@ -4,12 +4,14 @@ import {
   addWitness,
   updateWitness,
   getWitnesses,
+  getWitness,
 } from "../controllers/witness.controller.js";
 
 const witnessRouter = new Router();
 
 witnessRouter.post("/:id?", addWitness);
-witnessRouter.put("/case/:caseId/witnesses/:witnessId?", updateWitness);
-witnessRouter.get("/:id?", getWitnesses);
+witnessRouter.put("/:caseId/witnesses/:witnessId?", updateWitness);
+witnessRouter.get("/:caseId?", getWitnesses);
+witnessRouter.get("/:caseId/:witnessId?", getWitness);
 
 export default witnessRouter;
