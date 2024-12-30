03f61c18e5d6a2ee74a3549fa48fa65791812ba2
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-30 21:57:45 +0200

    Add: Delete All Coments API Route

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -5,14 +5,17 @@ import {
   getComments,
   getComment,
   likeComment,
+  deleteAllComments,
 } from "../controllers/comment.controller.js";
 
 const commentRouter = new Router();
 
 commentRouter.post("/:caseId?", createComment);
-commentRouter.delete("/:caseId/:commentId?", deleteComment);
+// commentRouter.delete("/:caseId/:commentId?", deleteComment);
 commentRouter.get("/:caseId?", getComments);
 commentRouter.get("/:caseId/:commentId?", getComment);
 commentRouter.post("/:caseId/:commentId?", likeComment);
+commentRouter.post("/:caseId/:commentId?", likeComment);
+commentRouter.delete("/:caseId", deleteAllComments);
 
 export default commentRouter;
