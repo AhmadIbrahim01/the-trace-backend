a2960f446008b3b137b41dfc3d368aee8b219a1a
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-30 03:30:34 +0200

    Add: Get Suspects Route

--- src/routes/suspect.routes.js
+++ src/routes/suspect.routes.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import {
   addSuspect,
   updateSuspect,
+  getSuspects,
 } from "../controllers/suspect.controller.js";
 const suspectRouter = new Router();
 
 suspectRouter.post("/:caseId?", addSuspect);
 suspectRouter.put("/:caseId/:suspectId?", updateSuspect);
+suspectRouter.get("/:caseId?", getSuspects);
 
 export default suspectRouter;
