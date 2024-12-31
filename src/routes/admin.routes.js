e48c2593d8e0277ef75ea60af94968d8b91c4684
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-31 04:45:25 +0200

    Add: Get Investigators API Route

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -5,10 +5,12 @@ import {
   toggleBanUser,
   getUsers,
   getUser,
+  getInvestigators,
 } from "../controllers/admin.controller.js";
 
 const adminRouter = new Router();
 
+adminRouter.get("/investigators", getInvestigators);
 adminRouter.get("/", getUsers);
 adminRouter.get("/:userId?", getUser);
 adminRouter.put("/:userId?", updateUser);
