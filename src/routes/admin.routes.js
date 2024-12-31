56064ffcb38ba202caaf2bcdf5b04333dc345069
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-31 04:33:42 +0200

    Add: Toggle Ban User API Route

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { updateUser, deleteUser } from "../controllers/admin.controller.js";
+import {
+  updateUser,
+  deleteUser,
+  toggleBanUser,
+} from "../controllers/admin.controller.js";
 
 const adminRouter = new Router();
 
 adminRouter.put("/:userId?", updateUser);
 adminRouter.delete("/:userId?", deleteUser);
+adminRouter.post("/ban/:userId?", toggleBanUser);
 
 export default adminRouter;
