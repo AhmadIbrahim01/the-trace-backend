a73b49f90b7f999b1925979688ab6dcc324a0f31
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-31 04:22:20 +0200

    Add: Update User API

---
+++ src/controllers/admin.controller.js
@@ -0,0 +1,87 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcrypt";
+
+export const updateUser = async (req, res) => {
+  const userId = req.params.userId;
+  const {
+    firstName,
+    lastName,
+    email,
+    phone,
+    password,
+    role,
+    profilePicture,
+    banned,
+  } = req.body;
+
+  try {
+    if (!firstName || !lastName) {
+      return res.status(400).send({
+        message: "Missing required fields",
+      });
+    }
+
+    const userData = await User.findById(userId);
+    if (!userData) {
+      return res.status(404).send({
+        message: "User not found",
+      });
+    }
+
+    if (email && email.toLowerCase() !== userData.email.toLowerCase()) {
+      const loweredEmail = email.toLowerCase();
+      const userEmail = await User.findOne({ email: loweredEmail });
+      if (userEmail) {
+        return res.status(409).send({
+          message: "Email already registered",
+        });
+      }
+    }
+
+    if (phone && phone !== userData.phone) {
+      const userPhone = await User.findOne({ phone });
+      if (userPhone) {
+        return res.status(409).send({
+          message: "Phone already registered",
+        });
+      }
+    }
+
+    let hashedPassword = userData.password;
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      hashedPassword = await bcrypt.hash(password, salt);
+    }
+
+    const updated = await User.findByIdAndUpdate(
+      userId,
+      {
+        firstName,
+        lastName,
+        email: email ? email.toLowerCase() : userData.email,
+        phone: phone || userData.phone,
+        password: hashedPassword,
+        role: role || userData.role,
+        profilePicture: profilePicture || userData.profilePicture,
+        banned: banned !== undefined ? banned : userData.banned,
+      },
+      { new: true }
+    );
+
+    if (!updated) {
+      return res.status(404).send({
+        message: "User not found",
+      });
+    }
+
+    return res.status(200).send({
+      message: "User updated successfully",
+      updated,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({
+      message: "Error happened",
+    });
+  }
+};
