2b7b8a215c6caac3c3d0237df1bf3fff07d4c98b
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-01 02:24:32 +0200

    Add: Add Suspect Statement API

---
+++ src/controllers/suspect.statements.controller.js
@@ -0,0 +1,77 @@
+import mongoose from "mongoose";
+import Case from "../models/case.model.js";
+
+export const addSuspectStatement = async (req, res) => {
+  const { caseId, suspectId } = req.params;
+
+  if (!caseId || !suspectId) {
+    return res.status(404).send({
+      message: "Missing case id or suspect id",
+    });
+  }
+
+  if (caseId && !mongoose.Types.ObjectId.isValid(caseId)) {
+    return res.status(400).send({
+      message: "Invalid case id format",
+    });
+  }
+  if (suspectId && !mongoose.Types.ObjectId.isValid(suspectId)) {
+    return res.status(400).send({
+      message: "Invalid suspect id format",
+    });
+  }
+
+  const { date, statement, photo, locationOfIncident } = req.body;
+
+  if (!date || !statement) {
+    return res.status(400).send({
+      message: "Incomplete data",
+    });
+  }
+  const parsedDate = new Date(date);
+  if (isNaN(parsedDate)) {
+    return res.status(400).send({
+      message: "Invalid date format",
+    });
+  }
+
+  try {
+    const caseData = await Case.findById(caseId);
+    if (!caseData) {
+      return res.status(404).send({
+        message: "Case not found",
+      });
+    }
+
+    const suspectIndex = caseData.suspects.findIndex(
+      (s) => s._id.toString() === suspectId
+    );
+    if (suspectIndex === -1) {
+      console.log("Suspect not found");
+    }
+
+    const suspect = caseData.suspects[suspectIndex];
+
+    const newStatement = {
+      date: parsedDate,
+      statement,
+      photo,
+      locationOfIncident,
+      createdAt: new Date(),
+    };
+    const statements = suspect.statements;
+    statements.push(newStatement);
+
+    await caseData.save();
+
+    return res.status(201).send({
+      message: "Statement added successfully",
+      newStatement,
+    });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).send({
+      message: "Error happened",
+    });
+  }
+};
