3a248e3e1c4d7bf6a676218bb82ac0436d06fe62
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-03 18:41:44 +0200

    Add: Get User Api Route Inside Of Auth Routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { register, login } from "../controllers/auth.controller.js";
+import { register, login, getUser } from "../controllers/auth.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const authRouter = new Router();
 
 authRouter.post("/login", login);
 authRouter.post("/register", register);
+authRouter.get("/:userId?", getUser);
 
 export default authRouter;
