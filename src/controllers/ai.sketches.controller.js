1b437b2f5311d6aaf73fba1c22b0858c90fe63ed
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-08 23:33:00 +0200

    Add: Generate AI Sketch API

--- src/controllers/ai.sketches.controller.js
+++ src/controllers/ai.sketches.controller.js
@@ -57,3 +57,70 @@ export const addSketch = async (req, res) => {
     });
   }
 };
+
+export const generateSketch = async (req, res) => {
+  const { caseId, sketchId } = req.params;
+  if (!caseId) {
+    return res.status(404).send({
+      message: "Case Id is not found",
+    });
+  }
+  if (caseId && !mongoose.Types.ObjectId.isValid(caseId)) {
+    return res.status(500).send({
+      message: "Case Id is not in a valid format",
+    });
+  }
+  if (!sketchId) {
+    return res.status(404).send({
+      message: "Sketch Id is not found",
+    });
+  }
+  if (sketchId && !mongoose.Types.ObjectId.isValid(sketchId)) {
+    return res.status(500).send({
+      message: "Sketch Id is not in a valid format",
+    });
+  }
+  const { prompt } = req.body;
+  if (!prompt) {
+    return res.status(404).send({
+      message: "Prompt is missing",
+    });
+  }
+
+  try {
+    const caseData = await Case.findById(caseId);
+    if (!caseData) {
+      return res.status(404).send({
+        message: "Case has not been found",
+      });
+    }
+
+    const sketchIndex = caseData.suspectSketches.findIndex(
+      (sketch) => sketch._id.toString() === sketchId
+    );
+    if (sketchIndex === -1) {
+      return res.status(404).send({
+        message: "Sketch not found",
+      });
+    }
+
+    const image = await openai.images.generate({
+      model: "dall-e-2",
+      prompt,
+      size: "256x256",
+    });
+
+    caseData.suspectSketches[sketchIndex] = image.data;
+    await caseData.save();
+
+    return res.status(201).send({
+      message: "API request successful",
+      url: image.data,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({
+      message: "Error happened",
+    });
+  }
+};
