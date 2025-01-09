936e0e63ca65cae151584f439d9667e8ccab44a3
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-09 03:05:41 +0200

    Add: Save Sketch API Route

--- src/routes/ai.sketch.routes.js
+++ src/routes/ai.sketch.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { generateTextSketch } from "../controllers/ai.sketches.controller.js";
+import {
+  generateTextSketch,
+  saveSketch,
+} from "../controllers/ai.sketches.controller.js";
 
 const sketchRouter = new Router();
 
 sketchRouter.post("/:caseId?", generateTextSketch);
+sketchRouter.post("/save/:caseId?", saveSketch);
 
 export default sketchRouter;
