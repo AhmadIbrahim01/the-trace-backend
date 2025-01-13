7a18a95702e3483363386e9d8ceb998a5bd247e8
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-13 06:57:03 +0200

    Add: Generate Image Sketch API Route

--- src/routes/ai.sketch.routes.js
+++ src/routes/ai.sketch.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   generateTextSketch,
   saveSketch,
+  generateImageSketch,
 } from "../controllers/ai.sketches.controller.js";
 
 const sketchRouter = new Router();
 
 sketchRouter.post("/save/:caseId?", saveSketch);
 sketchRouter.post("/:caseId?", generateTextSketch);
+sketchRouter.post("/image/:caseId?", generateImageSketch);
 
 export default sketchRouter;
