05b74b7cac08c880c754c3bbea325bf4a90fb5d5
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2025-01-14 04:56:59 +0200

    Add: Delete Chat API Inside Of Investigator GPT Controller

--- src/controllers/investigator.gpt.controller.js
+++ src/controllers/investigator.gpt.controller.js
@@ -93,9 +93,65 @@ export const updateChatName = async (req, res) => {
     console.log(error.message);
   }
 };
+export const deleteChat = async (req, res) => {
+  const { userId, chatId } = req.params;
+  if (!userId) {
+    return res.status(400).send({
+      message: "User ID is missing",
+    });
+  }
+  if (userId && !mongoose.Types.ObjectId.isValid(userId)) {
+    return res.status(400).send({
+      message: "User ID is of invalid format",
+    });
+  }
+  if (!chatId) {
+    return res.status(400).send({
+      message: "Chat ID is missing",
+    });
+  }
+  if (chatId && !mongoose.Types.ObjectId.isValid(chatId)) {
+    return res.status(400).send({
+      message: "Chat ID is of invalid format",
+    });
+  }
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).send({
+        message: "Usere not found",
+      });
+    }
+
+    const chats = user.chats;
+    const chatIndex = chats.findIndex((chat) => chat._id.toString() === chatId);
+    if (chatIndex === -1) {
+      return res.status(404).send({
+        message: "Chat not found",
+      });
+    }
+
+    const deletedChat = chats[chatIndex];
+
+    const updatedChats = chats.filter((chat) => chat._id.toString() !== chatId);
+
+    user.chats = updatedChats;
+    await user.save();
+
+    return res.status(200).send({
+      message: "Chat deleted successfully",
+      deletedChat,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({
+      message: "Error happened ",
+    });
+  }
+};
 
 export const getChat = (req, res) => {};
-export const deleteChat = (req, res) => {};
 
 export const getChats = (req, res) => {};
 
--- src/routes/investigator.gpt.routes.js
+++ src/routes/investigator.gpt.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   addChat,
   updateChatName,
+  deleteChat,
 } from "../controllers/investigator.gpt.controller.js";
 
 const investigatorGptRouter = new Router();
 
 investigatorGptRouter.post("/:userId?", addChat);
 investigatorGptRouter.put("/:userId/:chatId?", updateChatName);
+investigatorGptRouter.delete("/:userId/:chatId?", deleteChat);
 
 export default investigatorGptRouter;
